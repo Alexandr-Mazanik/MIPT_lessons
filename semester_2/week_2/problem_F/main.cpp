9143deaa70bed2e4877a81070ae556196279d7a5
Alexandr <alexandrmazanik@gmail.com>
2021-02-12 13:10:39 +0300

    pr_F

--- semester_2/week_2/problem_F/main.cpp
+++ semester_2/week_2/problem_F/main.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
-	
-	
+	int element, max_el = 0;
+	int num_of_max = 0;
+
+	do
+	{
+		cin >> element;
+		if (element > max_el)
+		{
+			num_of_max = 0;
+			max_el = element;
+		}
+		if (element == max_el)
+			num_of_max++;
+	} while (element != 0);
+
+	cout << endl << num_of_max << endl;
+
 	return 0;
 }
